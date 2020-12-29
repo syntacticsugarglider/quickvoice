d46f89099c6e9fd8ebf6cedcbe494537fe974c2f
Izzy Swart <zenerboson@gmail.com>
2020-12-29 13:09:36 -0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,39 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE)
+            .then((cache) => cache.add('index.html'))
+    );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+    workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith((async () => {
+            try {
+                const preloadResp = await event.preloadResponse;
+                if (preloadResp) {
+                    return preloadResp;
+                }
+                const networkResp = await fetch(event.request);
+                return networkResp;
+            } catch (error) {
+                const cache = await caches.open(CACHE);
+                const cachedResp = await cache.match('index.html');
+                return cachedResp;
+            }
+        })());
+    }
+});
